21e04dfc098a5c8577fffd6eddf719a1d435bc7f
liyu <xiamu.ly@raycloud.com>
2019-04-17 11:29:04 +0800

    冲突

--- service-hi/src/main/java/com/liyu/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/liyu/servicehi/ServiceHiApplication.java
@@ -50,6 +50,12 @@ public class ServiceHiApplication {
     public static void main(String[] args) {
 
     	System.out.println("bbbbbb");
+    	System.out.println("bbbbbb");
+    	System.out.println("bbbbbb");
+    	System.out.println("bbbbbb");
+    	System.out.println("bbbbbb");
+    	System.out.println("bbbbbb");
+    	System.out.println("bbbbbb");
 
     }
 }
