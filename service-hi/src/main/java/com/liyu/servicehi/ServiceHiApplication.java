d5ad54fae0f7aeab14885679333d6aaeaca9b995
liyu <xiamu.ly@raycloud.com>
2019-04-17 11:49:06 +0800

    冲突

--- service-hi/src/main/java/com/liyu/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/liyu/servicehi/ServiceHiApplication.java
@@ -49,13 +49,16 @@ public class ServiceHiApplication {
 
     public static void main(String[] args) {
 
-    	System.out.println("bbbbbb");
-    	System.out.println("bbbbbb");
-    	System.out.println("bbbbbb");
-    	System.out.println("bbbbbb");
-    	System.out.println("bbbbbb");
-    	System.out.println("bbbbbb");
-    	System.out.println("bbbbbb");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
+    	System.out.println("分支");
     	System.out.println("分支");
 
     }
